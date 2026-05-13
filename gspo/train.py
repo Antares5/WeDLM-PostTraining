b1ccf21fe3f1d990e880e2a51401c6d3d0dd9b6d
Antares5 <594689279@qq.com>
2026-05-13 11:01:16 +0800

    log_fix

--- gspo/train.py
+++ gspo/train.py
@@ -19,6 +19,8 @@ from src import GSPOTrainingConfig, GSPOTrainer
 logging.basicConfig(
     format="%(asctime)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+# Disable DEBUG logs from WeDLM generation internals.
+logging.getLogger("hf_compat.modeling_wedlm").setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
 
 
