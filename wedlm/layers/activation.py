f20e0102d0418ace7d9bb6afc7ff388290dd3426
Antares5 <594689279@qq.com>
2026-05-15 17:36:08 +0800

    compile_fix

--- wedlm/layers/activation.py
+++ wedlm/layers/activation.py
@@ -21,7 +21,7 @@ class SiluAndMul(nn.Module):
     def __init__(self):
         super().__init__()
 
-    @torch.compile
+    @torch.compile(dynamic=True)
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x, y = x.chunk(2, -1)
         return F.silu(x) * y
